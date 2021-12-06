f331c6ad6e036c317dbd2ce5d12119c36984bbdb
00butter <beei1004@naver.com>
2021-12-07 02:10:39 +0900

    알고리즘 part2 Array 정리

--- src/com/company/Study2.java
+++ src/com/company/Study2.java
@@ -253,7 +253,6 @@ public class Study2 {
             int n = in.nextInt();
             int[] arr = new int[n];
             for(int i=0;i<n; i++) arr[i] = in.nextInt();
-
             System.out.print(T.solution(n,arr)+" ");
         }
     }
